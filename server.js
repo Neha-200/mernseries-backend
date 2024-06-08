666307810ecbe4cd392397fec2742f40582b21cd
Neha-200 <144225203+Neha-200@users.noreply.github.com>
2024-06-09 00:02:55 +0530

    server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const errorMiddleware = require('./middlewares/error-middleware');
 const app = express();
 
 const corsOptions = {
-  origin: "http://localhost:5174",
+  origin: "https://my-reg-log-page.netlify.app",
   methods: "GET, POST, PUT, DELETE, PATCH, HEAD",
   credentials: true,
 }
